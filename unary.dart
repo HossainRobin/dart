748b8a31470aa4ccbf0d63422259553f1b3f3e5a
HossainRobin <hellorobinhossain@gmail.com>
2023-08-08 19:48:43 +0600

    unary operator

---
+++ unary.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 5;
+  int b = 10;
+
+  // a++;
+  // b--;
+
+  // print(++a);
+  // print(--b);
+
+  int preAdd = ++a;
+  int afterAdd = a++;
+
+  print("before adding plus: $preAdd");
+  print("after adding plus: $afterAdd");
+}
